9633f3726f400ba677fac2d016c585f21e0498b3
Anant Jamuar <jamuaranant@gmail.com>
2025-04-19 20:34:30 +0530

    change in the package.json

--- src/js/appwrite.js
+++ src/js/appwrite.js
@@ -2,8 +2,8 @@ import { Client, Account, ID } from 'appwrite';
 
 // Initialize Appwrite client with Appwrite Cloud
 const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('your-project-id'); // Replace with your Appwrite Cloud project ID
+    .setEndpoint('https://fra.cloud.appwrite.io/v1')
+    .setProject('68038b6a001ffa473b65'); // Replace with your Appwrite Cloud project ID
 
 // Initialize Appwrite account
 const account = new Account(client);
