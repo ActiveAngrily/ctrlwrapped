655f9e8f88ee333d178aca340b3ab165467be2c2
Anant Jamuar <jamuaranant@gmail.com>
2025-04-19 20:59:18 +0530

    bug fix, CORS issue

--- src/js/appwrite.js
+++ src/js/appwrite.js
@@ -1,9 +1,10 @@
-import { Client, Account, ID } from 'appwrite';
+// Use the global Appwrite object provided by the CDN
+const { Client, Account, ID } = Appwrite;
 
 // Initialize Appwrite client with Appwrite Cloud
 const client = new Client()
     .setEndpoint('https://fra.cloud.appwrite.io/v1')
-    .setProject('68038b6a001ffa473b65'); // Replace with your Appwrite Cloud project ID
+    .setProject('68038b6a001ffa473b65'); // Your project ID
 
 // Initialize Appwrite account
 const account = new Account(client);
